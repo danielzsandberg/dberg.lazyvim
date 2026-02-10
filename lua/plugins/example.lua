3fc968bb9d37035d74726dbf616cecede5a05eca
Daniel Sandberg <danielzsandberg@gmail.com>
2026-02-09 22:04:05 -0500

    more

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -1,18 +1,8 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {
-  { import = "lazyvim.plugins.extras.lang.typescript" },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      -- add tsx and treesitter
-      vim.list_extend(opts.ensure_installed, {
-        "tsx",
-        "typescript",
-      })
-    end,
-  },
-  } end
+if true then
+  return {}
+end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
